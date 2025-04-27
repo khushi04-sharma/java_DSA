ad4b789fd678e197b7335d50eba405d976abcbd6
khushi kumari <khushik10201@gmail.com>
2025-04-27 23:07:49 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,17 @@
+// public class JavaBasics{
+
+//            public static void main(String args[]){
+//         int a=10; int b=2;
+//         int c=a+b;
+//         System.out.print(c);
+//         //
+//         /* */
+//     }
+// }
+
+class demo
+{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
